46d06166665d210ee8c65b536295b7060ede76f3
Pavel Guliukin <naveq4358@gmail.com>
2023-05-02 17:32:03 +0500

    commit

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -7,12 +7,11 @@ from django.core.exceptions import ValidationError
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        # fields = '__all__'
         fields = ['title', 'content', 'is_published', 'category']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'choices': ''}),
 
         }
     def clean_title(self):
@@ -20,10 +19,3 @@ class NewsForm(forms.ModelForm):
         if re.match(r'\d', title):
             raise ValidationError('Давай без цифр брат;)')
         return title
-
-
-
-    # title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    # content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    # is_published = forms.BooleanField(label='Опубликовать?', initial=True)
-    # category = forms.ModelChoiceField(empty_label='Выберите категорию', queryset=Category.objects.all(), label='Категория', widget=forms.Select(attrs={'class': 'form-control'}))
